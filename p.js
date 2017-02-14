0a8dc229cd4cab1cbf189a9615769c87f1b62417
abhigupta14 <abhigupta15@yahoo.in>
2017-02-14 12:31:06 +0530

    gmm

---
+++ p.js
@@ -0,0 +1,14 @@
+var PSV = new PhotoSphereViewer({
+    panorama: 'https://raw.githubusercontent.com/JeremyHeleine/Photo-Sphere-Viewer/master/examples/sun.jpg',
+    container: 'photosphere',
+    loading_img: 'https://raw.githubusercontent.com/mistic100/Photo-Sphere-Viewer/3.1.0/example/photosphere-logo.gif',
+    navbar: 'autorotate zoom download fullscreen',
+    caption:'',
+    default_fov: 65,
+    mousewheel: false,
+    size: {
+      height: 400
+    }
+  }
+);
+
---
+++ p2.js
@@ -0,0 +1,13 @@
+var PSV = new PhotoSphereViewer({
+    panorama: 'https://i0.wp.com/www.samrohn.com/wp-content/uploads/jenna-lyons-office-jcrew-360-VR-panorama-002.jpg?fit=1600%2C800',
+    container: 'photosphere2',
+    loading_img: 'https://raw.githubusercontent.com/mistic100/Photo-Sphere-Viewer/3.1.0/example/photosphere-logo.gif',
+    navbar: 'autorotate zoom download fullscreen',
+    caption:'',
+    default_fov: 65,
+    mousewheel: false,
+    size: {
+      height: 400
+    }
+  }
+);
